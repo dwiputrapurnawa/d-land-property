6f70874b418d8de14fb38e3424d2543209aa89d6
Ida Bagus Dwi Putra Purnawa <putrapurnawa@gmail.com>
2025-01-19 22:52:25 +0800

    add error exception in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Middleware\SecureHeaders;
+use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use \Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +20,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(SecureHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (HttpException $exception, Request $request) {
+            if ($exception->getStatusCode() == 404) {
+                $company = Company::first();
+                return response()->view("errors.404", ["company" => $company], 404);
+            }
+        });
     })->create();
